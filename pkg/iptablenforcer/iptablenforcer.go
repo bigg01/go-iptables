191af3f3c6bedb6bacfb77bdebec0eb4f1be6c3c
bigg01 <o.guggenbuehl@gmail.com>
2019-07-14 17:16:20 +0200

    add first test

--- pkg/iptablenforcer/iptablenforcer.go
+++ pkg/iptablenforcer/iptablenforcer.go
@@ -18,17 +18,14 @@ func init() {
 		DisableColors: false,
 		FullTimestamp: true,
 	})
-  
+
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
 	log.SetOutput(os.Stdout)
-  
+
 	// Only log the warning severity or above.
 	log.SetLevel(log.InfoLevel)
-  }
-
-
-
+}
 
 func contains(list []string, value string) bool {
 	for _, val := range list {
@@ -39,16 +36,6 @@ func contains(list []string, value string) bool {
 	return false
 }
 
-
-type firewallRule struct {
-    chain string
-    rule []string
-}
-
-type firewallRules struct {
-    rules []firewallRule
-}
-
 /*
 OS_FIREWALL_ALLOW --> Input for NodeExporter OCP ETC
 OPENSHIFT-ADMIN-OUTPUT-RULES --> OUTPUT from POD
@@ -56,7 +43,7 @@ OPENSHIFT-ADMIN-OUTPUT-RULES --> OUTPUT from POD
 
 func ApplRules() {
 	ipt, err := iptables.New()
-	
+
 	chain := "GUO_OPENSHIFT_INPUT"
 	// Saving the list of chains before executing tests
 	// chain now exists	
@@ -66,14 +53,14 @@ func ApplRules() {
 	} else {
 		log.Infof("ClearChain done: %v", chain)
 	}
-	
+
 	listChain, err := ipt.ListChains("filter")
 	if err != nil {
 		fmt.Printf("ListChains of Initial failed: %v", err)
 	} else {
 		log.Infof("List Chain works: %v", chain)
 	}
-	
+
 	// check that chain is fully gone and that state similar to initial one
 	listChain, err = ipt.ListChains("filter")
 	if err != nil {
@@ -81,54 +68,54 @@ func ApplRules() {
 	} else {
 		log.Infof("List Chain works: %v", listChain)
 	}
-	
+
 	// put a simple rule in
 	/*
-	err = ipt.AppendUnique("filter", chain, "-s", "0/0", "-j", "ACCEPT")
-	if err != nil {
-		fmt.Printf("Append failed: %v", err)
-	}
+		err = ipt.AppendUnique("filter", chain, "-s", "0/0", "-j", "ACCEPT")
+		if err != nil {
+			fmt.Printf("Append failed: %v", err)
+		}
 	*/
 
 	//fwRuelSet :=  firewallRules{firewallRule{chain: "GUO_OPENSHIFT_INPUT", rule: []string{"-p", "tcp", "--dport", "22", "-m", "conntrack", "--ctstate", "NEW,ESTABLISHED", "-j", "ACCEPT", "-m", "comment", "--comment", "\"tkggo test\""}}
 
 	fwRules := firewallRule{chain: "GUO_OPENSHIFT_INPUT", rule: []string{"-p", "tcp", "--dport", "22", "-m", "conntrack", "--ctstate", "NEW,ESTABLISHED", "-j", "ACCEPT", "-m", "comment", "--comment", "\"tkggo test\""}}
 	fwRules1 := firewallRule{chain: "GUO_OPENSHIFT_INPUT", rule: []string{"-p", "tcp", "--dport", "23", "-m", "conntrack", "--ctstate", "NEW,ESTABLISHED", "-j", "ACCEPT", "-m", "comment", "--comment", "\"tkggo test 23\""}}
-	fwRuelSet := firewallRules{[]firewallRule{fwRules,fwRules1}}
+	fwRuelSet := firewallRules{[]firewallRule{fwRules, fwRules1}}
 
-/*
-	e, err := json.Marshal(fwRuelSet)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(string(e))
-*/
+	/*
+	   	e, err := json.Marshal(fwRuelSet)
+	       if err != nil {
+	           fmt.Println(err)
+	           return
+	       }
+	       fmt.Println(string(e))
+	*/
 
 	//fmt.Println(fwRuelSet.rules)
-	for _, rr := range fwRuelSet.rules  {
-	log.Infoln(rr.chain, rr.rule)
-	err = ipt.AppendUnique("filter", rr.chain, rr.rule...)
-	if err != nil {
-		log.Warnf("Append failed: %v", err)
-	} else {
-		log.Infof("LisAppend works: %v, %v", fwRules.chain, fwRules.rule)
-	}
+	for _, rr := range fwRuelSet.rules {
+		log.Infoln(rr.chain, rr.rule)
+		err = ipt.AppendUnique("filter", rr.chain, rr.rule...)
+		if err != nil {
+			log.Warnf("Append failed: %v", err)
+		} else {
+			log.Infof("LisAppend works: %v, %v", fwRules.chain, fwRules.rule)
+		}
 	}
 
 	/*
-	listChain, err = ipt.List("filter", chain)
-	if err != nil {
-		fmt.Printf("ListChains failed: %v", err)
-	}
-	fmt.Println("===>>")
-	fmt.Println(listChain)
-   */
+		listChain, err = ipt.List("filter", chain)
+		if err != nil {
+			fmt.Printf("ListChains failed: %v", err)
+		}
+		fmt.Println("===>>")
+		fmt.Println(listChain)
+	*/
 	stats, err := ipt.Stats("filter", chain)
 	if err != nil {
 		fmt.Printf("stats failed: %v", err)
 	}
 	fmt.Println("================>>")
 	fmt.Println(stats)
-	
+
 }
