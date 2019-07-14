6b89368101a66cc29d0096e81c2d65fe64822f5b
bigg01 <o.guggenbuehl@gmail.com>
2019-07-14 13:32:06 +0200

    update module

--- cmd/iptablenforcer/main.go
+++ cmd/iptablenforcer/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	//	"reflect"
 	"os"
 	//"encoding/json"
@@ -10,8 +10,26 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+	//&log.TextFormatter{
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: true,
+	})
+  
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+  
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+  }
+
 
 func main() {
-
+	log.Infof("Start Iptables enforcer done...")
 iptablenforcer.ApplRules()
+
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,8 @@
-module github.com/bigg01/go-iptables/pkg/iptablenforcer
+module github.com/bigg01/go-iptables
 
 go 1.12
 
 require (
-	github.com/bigg01/go-iptables v0.0.0-20190714111714-a8e185f00e7a
 	github.com/coreos/go-iptables v0.4.1
 	github.com/sirupsen/logrus v1.4.2
 )
